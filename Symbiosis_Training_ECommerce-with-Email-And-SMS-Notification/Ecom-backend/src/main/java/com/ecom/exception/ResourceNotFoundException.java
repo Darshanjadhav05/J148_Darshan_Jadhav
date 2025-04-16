15939ffdac3767623eccd350eeeef0d5cac2e940
Darshan Jadhav <114412247+Darshanjadhav05@users.noreply.github.com>
2025-04-16 19:58:35 +0530

    Delete Symbiosis_Training_ECommerce-with-Email-And-SMS-Notification/Ecom-backend directory

--- Symbiosis_Training_ECommerce-with-Email-And-SMS-Notification/Ecom-backend/src/main/java/com/ecom/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.ecom.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
